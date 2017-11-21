37040e2eb04765daface5428c6e09e087e66a75b
Alex <alexstlopes@gmail.com>
2017-11-20 21:04:23 -0300

    Aula 13

--- app/Customer.php
+++ app/Customer.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
+    protected $casts = [
+        'special_customer' => 'boolean',
+        'birthdate' => 'date',
+    ];
+
     public function purchases()
     {
         return $this->hasMany(Purchases::class);
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,22 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //LISTAR CUSTOMERS
+    //$customers = Teste\Customer::paginate(10);
+    //dd($customers);
+    //echo $customers->render();
+
+    //CRIAR NOVO CUSTOMER
+    //$customer = new Teste\Customer();
+    //$customer->name = 'Fabio Vedovelli';
+    //$customer->city = 'Santo Andre';
+    //$customer->state = 'SP';
+    //$customer->special_customer = true;
+    //$customer->birthdate = '1974-08-07 00:00:00';
+    //$customer->save();
+    //dd($customer->toArray());
+
+    //TRAZER CUSTOMER ID 2
+    $customer = Teste\Customer::find(1);
+    dd($customer->birthdate);
 });
